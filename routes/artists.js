5cc0811fb50ef3206421fd79f574e235d87f8e89
aerii <aerii@live.com>
2016-02-04 19:15:54 -0800

    updated taylorswift

--- routes/artists.js
+++ routes/artists.js
@@ -1,3 +1,10 @@
+// Get all of our friend data
+var data = require('../data.json');
+
+exports.view = function(req, res) {
+	res.render('artists', data);
+};
+
 exports.viewArtists = function(req, res) {
 	var name = req.params.name;
 	//console.log("This project has a name of: "+name);
