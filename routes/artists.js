1f416fa5cea1b65c71406a7d3898cf3760d65050
Kyle Zhu <kyzhu@ucsd.edu>
2016-02-18 21:02:18 -0800

    updated artists pages

--- routes/artists.js
+++ routes/artists.js
@@ -1,3 +1,13 @@
 exports.view = function(req, res){
   res.render('artists');
 };
+
+exports.viewTaylorSwift = function(req, res){
+	res.render('taylorswift');
+};
+exports.viewPorterRobinson = function(req, res){
+	res.render('porterrobinson');
+};
+exports.viewKanyeWest = function(req, res){
+	res.render('kanyewest');
+};
