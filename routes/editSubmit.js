a8f46b374030f5442cdc37a3aabe67a4b54d62c5
Kyle Zhu <kyzhu@ucsd.edu>
2016-02-25 12:28:41 -0800

    trying to fix profile page removal info

--- routes/editSubmit.js
+++ routes/editSubmit.js
@@ -4,6 +4,35 @@ exports.pushProfile = function (req, res){
     MongoClient.connect('mongodb://admin:admin@ds013908.mongolab.com:13908/heroku_qmhr9291', function(err,db) {
         if(err) throw err;
 
+        // db.collection('Profile', function(err, collection) { if (!err) {
+        //         // collection.update(
+        //         //     {"date": req.body.date,
+        //         //      "user": req.body.user,
+        //         //      "aboutme": req.body.aboutme,
+        //         //      "fav1": req.body.fav1,
+        //         //      "fav2": req.body.fav2,
+        //         //      "fav3": req.body.fav3,
+        //         //      "gen1": req.body.gen1,
+        //         //      "gen2": req.body.gen2,
+        //         //      "gen3": req.body.gen3
+        //         // 	});                
+
+        //         db.close();
+        //     } else {
+        //         throw err;
+        //     }
+        //     });
+
+        // db.collection.find({"user": "temporary"}).delete();
+        if (!err) {
+            db.collection('Profile', function (err, collection) {
+                if (!err) {
+                    collection.findOne().remove();
+                    console.log("SUCCESSFULLY REMOVED FROM PROFILE");
+                }
+            });
+        }
+
         db.collection('Profile').insert(req.body);
     });
 
--- routes/profile.js
+++ routes/profile.js
@@ -3,5 +3,22 @@
  */
 
 exports.view = function(req, res){
-  res.render('profile');
+  // res.render('profile');
+  var MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect('mongodb://admin:admin@ds013908.mongolab.com:13908/heroku_qmhr9291', function (err, db) {
+        if (!err) {
+            db.collection('Profile', function (err, collection) {
+                if (!err) {
+                    collection.find({"user": "temporary"}).toArray(function(err, results) {
+                        db.close();
+                        res.render('profile', {'profileinfo': results[0]});
+                    }); 
+                }
+            })
+        }
+    });
+
+
 };
+
+
